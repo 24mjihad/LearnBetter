8c50698f19f464d5430073f2b39433d2fe52eb71
Mohammad Jihad <76707765+24mjihad@users.noreply.github.com>
2022-07-24 22:44:53 -0400

    lots of progress made

--- scripts.js
+++ scripts.js
@@ -1,24 +1,76 @@
-const RANDOM_QUOTE_API_URL = 'https://api.quotable.io/random'
-const quoteDisplayElement = document.getElementById('quoteDisplay')
-const quoteInputElement = document.getElementById('quoteInput')
-
-
-function getRandomQuote() {
-    return fetch(RANDOM_QUOTE_API_URL)
-        .then(Response => Response.json())
-        .then(data => data.content)
+function addClass(el,name){
+el.className += ' '+name;
 }
 
-async function renderNewQuote() {
-    const quote= await getRandomQuote()
-    quoteDisplayElement.innerHTML = ''
-    quote.split('').forEach(character => {
-    const characterSpan = document.createElement('spam')
-    characterSpan.innerText= character
-    quoteDisplayElement.appendChild(characterSpan)
-    })
-    quoteInputElement.value = none
+function removeClass(el,name) {
+el.className = el.className.replace(name,'');
 }
 
-renderNewQuote()
 
+const words = 'emerged during this period and influenced China and its neighbors for centuries to come. In the third century BCE, Qins wars of unification created the first Chinese empire, the short-lived Qin dynasty'.split(" ");
+wordcount = words.length;
+function formatWord(word) {
+    return `<div class="word"><span class="letter">${word.split('').join(`</span><span class='letter'>`)}</div>`;
+}
+
+
+function newGame() {
+    adder = 0
+    document.getElementById('words').innerHTML = '';
+    for (let i=0; i<wordcount; i++) {
+        document.getElementById('words').innerHTML += formatWord(words[adder]);
+        adder++;
+    }
+    addClass(document.querySelector('.word'), 'current');
+    addClass(document.querySelector('.letter'), 'current');
+}
+document.getElementById('game').addEventListener('keyup', ev => {
+    const key = ev.key;
+    const currentWord = document.querySelector('.word.current')
+    const currentLetter = document.querySelector('.letter.current')
+    const expected = currentLetter?.innerHTML || ' ';
+    const isLetter = key.length === 1 && key !== ' ';
+    const isSpace = key === ' ';
+
+    console.log({key,expected});
+
+    if (isLetter) {
+        if(currentLetter) {
+            addClass(currentLetter, key === expected ? 'correct' : 'incorrect');
+            removeClass(currentLetter, 'current');
+            if (currentLetter.nextSibling) {
+                addClass(currentLetter.nextSibling, 'current')  
+            }
+        } else {
+            const incorrectLetter = document.createElement('span');
+            incorrectLetter.innerHTML = key;
+            incorrectLetter.className = 'letter incorrect extra';
+            currentWord.appendChild(incorrectLetter);
+        }
+    }
+if (isSpace) {
+    if(expected !== ' ') {
+        const lettersToInvalidate = [...document.querySelectorAll('.word.current .letter:not(.correct)')]
+        lettersToInvalidate.forEach(letter => {
+            addClass(letter, 'incorrect');
+        });
+    }
+    removeClass(currentWord, 'current');
+    addClass(currentWord.nextSibling, 'current');
+    if (currentLetter) {
+        removeClass(currentLetter, 'current');
+    }
+    addClass(currentWord.nextSibling.firstChild, 'current');
+}
+
+// move cursor
+const nextLetter = document.querySelector('.letter.current');
+const nextWord = document.querySelector('.word.current');
+const cursor = document.getElementById('cursor');
+cursor.style.top = (nextLetter || nextWord).getBoundingClientRect().top+ 2+  'px';
+cursor.style.left =  (nextLetter || nextWord).getBoundingClientRect()[nextLetter ? 'left' : 'right']+  'px';
+
+})
+
+
+newGame()
