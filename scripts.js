3af3e5f2a6258d9ee7a2efd593b4820e950cdc5f
Mohammad Jihad <Jihadu2006@gmail.com>
2022-08-29 12:45:17 -0400

    lint

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
-function addClass(el,name){
-el.className += ' '+name;
+function addClass(el, name) {
+    el.className += ' ' + name;
 }
 
-function removeClass(el,name) {
-el.className = el.className.replace(name,'');
+function removeClass(el, name) {
+    el.className = el.className.replace(name, '');
 }
 
 
@@ -22,7 +22,7 @@ function formatWord(word) {
 function newGame() {
     adder = 0
     document.getElementById('words').innerHTML = '';
-    for (let i=0; i<wordcount; i++) {
+    for (let i = 0; i < wordcount; i++) {
         document.getElementById('words').innerHTML += formatWord(words[adder]);
         adder++;
     }
@@ -35,13 +35,13 @@ function getWpm() {
     var lastTypeWord = document.querySelector('.word.current');
     var lastTypeWordIndex = words.indexOf(lastTypeWord);
     var typedWords = words.slice(0, lastTypeWordIndex)
-    var correctWords = typedWords.filter(word =>{
+    var correctWords = typedWords.filter(word => {
         const letters = [...word.children];
         const incorrectLetters = letters.filter(letter => letter.className.includes('incorrect'));
         const correctLetters = letters.filter(letter => letter.className.includes('correct'));
         return incorrectLetters.length === 0 && correctLetters.length === letters.length;
     })
-    return  correctWords.length/gameTime * 60000;
+    return correctWords.length / gameTime * 60000;
 }
 
 function gameOver() {
@@ -58,44 +58,44 @@ document.getElementById('game').addEventListener('keyup', ev => {
     const isSpace = key === ' ';
     const isBackSpace = key === 'Backspace';
     const isFirstLetter = currentLetter === currentWord.firstChild;
-    
-    
+
+
     if (document.querySelector('#game.Over')) {
         return;
     }
 
 
-    console.log({key,expected});
+    console.log({ key, expected });
 
-if (!window.timer && isLetter) {
-    window.timer = setInterval(() => {
-        if (!window.gameStart) {
-            window.gameStart = (new Date()).getTime();
-        }
-        const currentTime = (new Date()).getTime();
-        const msPassed = currentTime - window.gameStart;
-        const sPassed = Math.round(msPassed / 1000);
-        document.getElementById('info').innerHTML = sPassed + '';
-    }, 1000)
-}
-var words = [...document.querySelectorAll('.word')];
-var lastTypeWord = document.querySelector('.word.current');
-var lastTypeWordIndex = words.indexOf(lastTypeWord);
-var typedWords = words.slice(0, lastTypeWordIndex)
-//currentWord === lastWord && currentLetter === lastLetter
-const lastWord = words[(words.length) - 1];
-const lastLetter = lastWord.charAt((lastWord.length) -1)
+    if (!window.timer && isLetter) {
+        window.timer = setInterval(() => {
+            if (!window.gameStart) {
+                window.gameStart = (new Date()).getTime();
+            }
+            const currentTime = (new Date()).getTime();
+            const msPassed = currentTime - window.gameStart;
+            const sPassed = Math.round(msPassed / 1000);
+            document.getElementById('info').innerHTML = sPassed + '';
+        }, 1000)
+    }
+    var words = [...document.querySelectorAll('.word')];
+    var lastTypeWord = document.querySelector('.word.current');
+    var lastTypeWordIndex = words.indexOf(lastTypeWord);
+    var typedWords = words.slice(0, lastTypeWordIndex)
+    //currentWord === lastWord && currentLetter === lastLetter
+    const lastWord = words[(words.length) - 1];
+    const lastLetter = lastWord.charAt((lastWord.length) - 1)
 
-if  (currentWord.lastChild = 'd') {
-    console.log((lastTypeWord));
-}
+    if (currentWord.lastChild = 'd') {
+        console.log((lastTypeWord));
+    }
 
-if (isLetter) {
-        if(currentLetter) {
+    if (isLetter) {
+        if (currentLetter) {
             addClass(currentLetter, key === expected ? 'correct' : 'incorrect');
             removeClass(currentLetter, 'current');
             if (currentLetter.nextSibling) {
-                addClass(currentLetter.nextSibling, 'current')  
+                addClass(currentLetter.nextSibling, 'current')
             }
         } else {
             const incorrectLetter = document.createElement('span');
@@ -104,49 +104,49 @@ if (isLetter) {
             currentWord.appendChild(incorrectLetter);
         }
     }
-if (isSpace) {
-    if(expected !== ' ') {
-        const lettersToInvalidate = [...document.querySelectorAll('.word.current .letter:not(.correct)')]
-        lettersToInvalidate.forEach(letter => {
-            addClass(letter, 'incorrect');
-        });
-    }
-    removeClass(currentWord, 'current');
-    addClass(currentWord.nextSibling, 'current');
-    if (currentLetter) {
-        removeClass(currentLetter, 'current');
-    }
-    addClass(currentWord.nextSibling.firstChild, 'current');
-}
-
-if (isBackSpace) {
-    if(currentLetter && isFirstLetter) {
+    if (isSpace) {
+        if (expected !== ' ') {
+            const lettersToInvalidate = [...document.querySelectorAll('.word.current .letter:not(.correct)')]
+            lettersToInvalidate.forEach(letter => {
+                addClass(letter, 'incorrect');
+            });
+        }
         removeClass(currentWord, 'current');
-        addClass(currentWord.previousSibling, 'current');
-        removeClass(currentLetter, 'current');
-        addClass(currentWord.previousSibling.lastChild, 'current')
-        removeClass(currentWord.previousSibling.lastChild, 'incorrect');
-        removeClass(currentWord.previousSibling.lastChild, 'correct');
+        addClass(currentWord.nextSibling, 'current');
+        if (currentLetter) {
+            removeClass(currentLetter, 'current');
+        }
+        addClass(currentWord.nextSibling.firstChild, 'current');
     }
-    if(currentLetter && !isFirstLetter) {
-        removeClass(currentLetter, 'current');
-        addClass(currentLetter.previousSibling, 'current');
-        removeClass(currentLetter.previousSibling, 'incorrect');
-        removeClass(currentLetter.previousSibling, 'correct');
-    }
-    if (!currentLetter) {
-        addClass(currentWord.lastChild, 'current');
-        removeClass(currentWord.lastChild, 'incorrect');
-        removeClass(currentWord.lastChild, 'correct');
-    }
-}
 
-// move cursor
-const nextLetter = document.querySelector('.letter.current');
-const nextWord = document.querySelector('.word.current');
-const cursor = document.getElementById('cursor');
-cursor.style.top = (nextLetter || nextWord).getBoundingClientRect().top+ 2+  'px';
-cursor.style.left =  (nextLetter || nextWord).getBoundingClientRect()[nextLetter ? 'left' : 'right']+  'px';
+    if (isBackSpace) {
+        if (currentLetter && isFirstLetter) {
+            removeClass(currentWord, 'current');
+            addClass(currentWord.previousSibling, 'current');
+            removeClass(currentLetter, 'current');
+            addClass(currentWord.previousSibling.lastChild, 'current')
+            removeClass(currentWord.previousSibling.lastChild, 'incorrect');
+            removeClass(currentWord.previousSibling.lastChild, 'correct');
+        }
+        if (currentLetter && !isFirstLetter) {
+            removeClass(currentLetter, 'current');
+            addClass(currentLetter.previousSibling, 'current');
+            removeClass(currentLetter.previousSibling, 'incorrect');
+            removeClass(currentLetter.previousSibling, 'correct');
+        }
+        if (!currentLetter) {
+            addClass(currentWord.lastChild, 'current');
+            removeClass(currentWord.lastChild, 'incorrect');
+            removeClass(currentWord.lastChild, 'correct');
+        }
+    }
+
+    // move cursor
+    const nextLetter = document.querySelector('.letter.current');
+    const nextWord = document.querySelector('.word.current');
+    const cursor = document.getElementById('cursor');
+    cursor.style.top = (nextLetter || nextWord).getBoundingClientRect().top + 2 + 'px';
+    cursor.style.left = (nextLetter || nextWord).getBoundingClientRect()[nextLetter ? 'left' : 'right'] + 'px';
 
 })
 
